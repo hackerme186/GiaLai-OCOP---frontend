78eff173316d04e3927f86df4798fe78683336b1
hackerme186 <122286397+hackerme186@users.noreply.github.com>
2025-10-05 17:50:18 +0700

    Update SocialLogin.tsx

--- src/components/SocialLogin.tsx
+++ src/components/SocialLogin.tsx
@@ -13,13 +13,13 @@ export default function SocialLogin() {
           type="button"
           className="inline-flex items-center justify-center rounded-lg border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50"
         >
-          Email link
+          Google
         </button>
         <button
           type="button"
           className="inline-flex items-center justify-center rounded-lg border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50"
         >
-          SMS
+          Facebook
         </button>
       </div>
     </>
